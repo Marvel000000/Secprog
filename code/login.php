b8edf4135419fd3a42188c864e9efaf69bd828a6
johnorlond <129138028+johnorlond@users.noreply.github.com>
2023-11-21 21:22:28 +0700

    code login, register, update dan dashboard temp

---
+++ code/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../style/login.css">
+</head>
+<body>
+
+    <form action="../controllers/loginAuth.php" method="post">
+        <h1>Login</h1>
+        <label for="email">Email:</label>
+        <input type="email" name="email" required>
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" required>
+
+        <p>Don't have an account? <a href="register.php">Register here</a></p>
+
+        <input type="submit" value="Login">
+    </form>
+
+</body>
+</html>
