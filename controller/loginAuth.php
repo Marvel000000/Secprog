5f8a9ff7ccc62a0469c1dcc4702fbf0f7ffa109b
johnorlond <129138028+johnorlond@users.noreply.github.com>
2023-11-22 20:32:01 +0700

    auth login, regis, update

---
+++ controller/loginAuth.php
@@ -0,0 +1,90 @@
+<?php
+require_once "./connection.php";
+
+// Initialize the session
+session_start();
+
+// Check if the user is already logged in, redirect to dashboard if true
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+    header("location: ../code/dashboard.php");
+    exit;
+}
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Validate user input
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    // Prepare and execute the SQL statement
+    $stmt = $conn->prepare("SELECT id, email, password FROM users WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $stmt->store_result();
+
+    // Check if the user exists
+    if ($stmt->num_rows > 0) {
+        // Bind the results
+        $stmt->bind_result($user_id, $username, $hashed_password);
+        $stmt->fetch();
+
+        // Verify the password
+        if (password_verify($password, $hashed_password)) {
+            // Password is correct, start a new session
+
+            // Check for double login (delete old session and add new session)
+            $double_login_stmt = $conn->prepare("SELECT sessionID FROM active_sessions WHERE username = ?");
+            $double_login_stmt->bind_param("s", $username);
+            $double_login_stmt->execute();
+            $double_login_stmt->store_result();
+
+            if ($double_login_stmt->num_rows > 0) {
+                // Delete the old session information
+                $double_login_stmt->bind_result($old_session_id);
+                $double_login_stmt->fetch();
+
+                $delete_old_session_stmt = $conn->prepare("DELETE FROM active_sessions WHERE sessionID = ?");
+                $delete_old_session_stmt->bind_param("s", $old_session_id);
+                $delete_old_session_stmt->execute();
+                $delete_old_session_stmt->close();
+            }
+
+            // Store data in session variables
+            $_SESSION["loggedin"] = true;
+            $_SESSION["id"] = $user_id;
+            $_SESSION["username"] = $username;
+            $_SESSION["email"] = $email;
+
+            // Store session information in the active_sessions table
+            $session_temp = session_id();
+            $session_insert_stmt = $conn->prepare("INSERT INTO active_sessions (sessionID, username, last_login) VALUES (?, ?, NOW())");
+            $session_insert_stmt->bind_param("ss", $session_temp, $username);
+            $session_insert_stmt->execute();
+            $session_insert_stmt->close();
+
+            // Debugging output
+            echo "Login successful. Redirecting to dashboard...";
+            header("location: ../code/dashboard.php");
+            exit;
+        } else {
+            $login_error = "Invalid password";
+        }
+    } else {
+        $login_error = "User not found";
+    }
+
+    $stmt->close();
+}
+
+// If there's a login error, redirect to login page with the error message
+if (isset($login_error)) {
+    header("location: ../code/login.php?error=$login_error");
+    exit;
+}
+
+// Debugging output
+echo "Login failed. Redirecting to login page...";
+
+// Close the database connection
+$conn->close();
+?>
---
+++ controller/regAuth.php
@@ -0,0 +1,105 @@
+<?php
+require_once "./connection.php";
+
+session_start(); // Start the session
+
+function is_valid_email($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+function is_valid_password($password) {
+    return preg_match('/^(?=.*\d)(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9]).{8,}$/', $password);
+}
+
+function is_valid_image($file) {
+    $allowedExtensions = array("jpeg", "jpg", "png", "gif");
+
+    if ($file["size"] > 50 * 1024 * 1024) { // 50 MB limit
+        return false;
+    }
+
+    if ($file["size"] == 0) {
+        return false;
+    }
+
+    $fileInfo = pathinfo($file["name"]);
+    $extension = strtolower($fileInfo["extension"]);
+
+    $imageType = exif_imagetype($file["tmp_name"]);
+    if ($imageType === false) {
+        return false;
+    }
+
+    $imageExtension = image_type_to_extension($imageType, false);
+
+    return in_array($imageExtension, $allowedExtensions);
+}
+
+function validateUsername($name) {
+    return !empty($name) && strlen($name) <= 30 && !preg_match('/[\<\>\;\:\"\'\%]/', $name);
+}
+
+// Initialize error array
+$errors = array();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Validate name
+    $name = $_POST['name'];
+    if (!validateUsername($name)) {
+        $errors[] = "Invalid name.";
+    }
+
+    // Validate email
+    $email = $_POST['email'];
+    if (empty($email) || strlen($email) > 30 || preg_match('/[\<\>\;\:\"\'\%]/', $email) || !is_valid_email($email)) {
+        $errors[] = "Invalid email.";
+    }
+
+    // Validate password
+    $password = $_POST['password'];
+    if (empty($password) || strlen($password) > 30 || preg_match('/[\<\>\;\:\"\'\%]/', $password) || !is_valid_password($password)) {
+        $errors[] = "Invalid password.";
+    }
+
+    // Validate image
+    $image = $_FILES["image"];
+    if (!is_valid_image($image)) {
+        $errors[] = "Invalid image.";
+    }
+
+    // If there are validation errors, store them in a session variable and redirect back to the registration form
+    if (!empty($errors)) {
+        $_SESSION['registration_errors'] = $errors;
+        header("location: ../code/register.php");
+        exit;
+    } else {
+        // If no validation errors, proceed with database insertion
+
+        $stmt = $conn->prepare("INSERT INTO users (id, name, email, password, image) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssss", $id, $name, $email, $password, $image_name);
+
+        $id = uniqid();
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        
+        // Generate a random string for the image name
+        $image_name = uniqid('', true) . '.' . pathinfo($image["name"], PATHINFO_EXTENSION);
+
+        if ($stmt->execute()) {
+            $target_dir = "../images/";
+            $target_file = $target_dir . $image_name;
+            move_uploaded_file($image["tmp_name"], $target_file);
+
+            // Redirect to login after successful insertion
+            header("location: ../code/login.php");
+            exit;
+        } else {
+            error_log("Error: " . $stmt->error);
+            echo "Error during database insertion. Check the logs for more details.";
+        }
+
+        $stmt->close();
+    }
+}
+?>
