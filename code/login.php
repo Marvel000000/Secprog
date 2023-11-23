c314aa7fc542b9b4df3116124d2cf8f98455bc9b
johnorlond <129138028+johnorlond@users.noreply.github.com>
2023-11-23 09:01:04 +0700

    generate csrf token

--- code/login.php
+++ code/login.php
@@ -1,14 +1,36 @@
+<?php
+require_once "../controller/connection.php";
+
+// Initialize the session
+session_start();
+
+// Function to generate a CSRF token
+function generateCSRFToken()
+{
+    return bin2hex(random_bytes(32));
+}
+
+// Generate and store CSRF token
+$csrf_token = generateCSRFToken();
+$_SESSION["csrf_token"] = $csrf_token;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="../styles/login.css">
 </head>
+
 <body>
 
     <form action="../controller/loginAuth.php" method="post">
+        <!-- Add the CSRF token as a hidden input field -->
+        <input type="hidden" name="csrf_token" value="<?php echo $csrf_token; ?>">
+
         <h1>Login</h1>
         <label for="email">Email:</label>
         <input type="email" name="email" required>
@@ -22,4 +44,5 @@
     </form>
 
 </body>
+
 </html>
